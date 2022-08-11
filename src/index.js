a5d031b3e302fc5b69ad9f7fe8fa18cb0b316492
bravo9798 <bhuvneshsharma9798@gmail.com>
2022-08-11 19:16:35 +0530

    created redux slice

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import store from "./redux/store";
 
+let persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
